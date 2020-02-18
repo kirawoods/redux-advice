a3c5fe4a973cc20844cb42cd5394e3c96313edef
Kira <kawoods826@gmail.com>
2020-02-18 09:22:14 -0800

    connects advice button and implements map state to props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { AdviceButton } from "./components/AdviceButton";
+import AdviceButton from "./components/AdviceButton";
 import AdviceSlip from "./components/AdviceSlip";
 
 function App() {
--- src/components/AdviceButton.js
+++ src/components/AdviceButton.js
@@ -1,6 +1,16 @@
 import React from "react";
-// import { connect } from "react-redux";
+import { connect } from "react-redux";
 
-export const AdviceButton = () => {
+const AdviceButton = () => {
   return <button>Consult the Oracle</button>;
 };
+
+const mapStateToProps = state => {
+  return {
+    advice: state.advice,
+    isLoading: false,
+    failed: false
+  };
+};
+
+export default connect(mapStateToProps, {})(AdviceButton);
