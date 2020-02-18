a582c78f0608e0e9ca0ef4bd1b9340b77b3dc9d8
Kira <kawoods826@gmail.com>
2020-02-17 21:10:07 -0800

    add dependencies and imports for implementing redux store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+function reducer() {
+  return {
+    title: "Hello world! I'm in the Redux store!"
+  };
+}
+
+const store = createStore(reducer, applyMiddleware(thunk));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
